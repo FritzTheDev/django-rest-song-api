2c5bbc16dd10db02fc504c48486d00e772832928
Frederick Johnson <fritzj96@gmail.com>
2019-11-03 20:01:33 -0800

    fixed broken import

--- api/music/tests.py
+++ api/music/tests.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
-from .models import Songs
+from models import Songs
 from .serializers import SongsSerializer
 
 # tests for views

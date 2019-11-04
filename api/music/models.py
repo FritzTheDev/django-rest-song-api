bb13a809014e12952e8929ece45833bef8d8b0d7
Frederick Johnson <fritzj96@gmail.com>
2019-11-03 19:58:15 -0800

    added Songs model

--- api/music/models.py
+++ api/music/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Songs(models):
+  # song title
+  title = models.CharField(max_length=255, null=False)
+  # artist name
+  artist = models.CharField(max_length=255, null=False)
+
+  def __str__(self):
+    return "{} - {}".format(self.title, self.artist)
